a4000c310aac340e6123950ca1986b8475da9d87
Silas Vergilio <silas.vergilio@gmail.com>
2023-01-19 01:27:06 -0300

    feat/hover

--- dnc-store/js/custom.js
+++ dnc-store/js/custom.js
@@ -1,33 +1,13 @@
-function alternarDestaque()
-{
+function alternarDestaque() {
     var cards = document.getElementsByClassName("card-produto");
     cards = Array.from(cards);
 
-    for(let i = 0; i < cards.length; i++)
-    {
+    for (let i = 0; i < cards.length; i++) {
         cards[i].classList.toggle('card-produto-destaque');
     }
 }
 
 
 
-function getProducts()
-{
-    fetch('http://localhost:3000/produtos').then( (response) => response.json()).then((dados) => {
 
-        var list = document.getElementById("product-list");
-        for(let i = 0; i < dados.length; i++){
-        
-        list.innerHTML = list.innerHTML + ` <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-        <div class="sport_product card-produto">
-           <figure><img width="250px" height="20px" src=${dados[i].urlImagem} alt="img" /></figure>
-           <h3><strong class="price_text">${dados[i].preco}</strong></h3>
-           <h4>${dados[i].nomeProduto}</h4>
-        </div>
-     </div>`
-        }
-    })
-}
-
-getProducts();
 setInterval(alternarDestaque, 1500);
